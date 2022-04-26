66d1d908b6228214c9ef52440e02197b4bbecfd0
Abhijeet <srivastavaabhijeet68@gmail.com>
2022-04-26 21:58:23 +0530

    New React 3

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 const ProductScreen = () => {
-    const params = useParams();
-    const product = products.find((p) => p._id === params.id);
+    const Params = useParams();
+    const [product, setProduct] = useState({})
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${Params.id}`)
 
+            setProduct(data);
+
+        }
+        fetchProduct()
+    }, [Params.id])
     return (
         <>
-            <Link className='btn btn-dark my-3' to = '/'>Go Back</Link>
+            <Link className='btn btn-dark my-3' to='/'>Go Back</Link>
             <Row>
                 <Col md={6}>
-                    <Image src = {product.image} alt = {product.name}/>
+                    <Image src={product.image} alt={product.name} />
                 </Col>
                 <Col md={3}>
                     <ListGroup variant='flush'>
@@ -20,7 +28,7 @@ const ProductScreen = () => {
                             <h3>{product.name}</h3>
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+                            <Rating value={product.rating} text={`${product.numReviews} reviews`} />
                         </ListGroup.Item>
                         <ListGroup.Item>
                             Price:${product.price}
