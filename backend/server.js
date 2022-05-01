c1ed885249f9b0a6f2f89be0ba4ad4db86e12533
Abhijeet <srivastavaabhijeet68@gmail.com>
2022-05-01 19:49:40 +0530

    React App

--- backend/server.js
+++ backend/server.js
@@ -3,27 +3,24 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 // import products from './data/products.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 dotenv.config()
 
 connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-// app.get('/api/products', (req, res) => {
-//     res.json(products)  
-// })
-// app.get('/api/products/:id', (req, res) => {
-//     const product = products.find((p) => p._id === req.params.id)
-//     res.json(product)  
-// })
-
-
 app.use('/api/products',productRoutes)
-
+app.use('/api/users',userRoutes)
+app.use('/api/orders', orderRoutes)
+app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
 const PORT = process.env.PORT || 5000
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,25 +1,44 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap'
+import { logout } from '../actions/userActions';
 const Header = () => {
+    const dispatch = useDispatch()
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
     return (
         <header>
             <Navbar bg="primary" variant="dark">
                 <Container>
                     <Navbar.Brand as={Link} to="/">Ganpati</Navbar.Brand>
                     <Nav className="mr-auto">
-                        <Nav.Link as = {Link} to="/login">
-                            <i className='fas fa-user'></i>
-                            Sign In
-                        </Nav.Link>
-                        <Nav.Link as = {Link} to="/cart">
-                            <i className='fas fa-shopping-cart'></i>
-                            Cart
-                        </Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-        </header>
+                        {userInfo ? (
+                            <NavDropdown title={userInfo.name} id='username'>
+                                <LinkContainer to ='/profile'>
+                                    <NavDropdown.Item>
+                                        Profile
+                                    </NavDropdown.Item>
+                                </LinkContainer>
+                                <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                            </NavDropdown>
+                        ): <Nav.Link as = {Link} to="/login">
+                        <i className='fas fa-user'></i>
+                        Sign In
+                    </Nav.Link>}
+
+                    <Nav.Link as={Link} to="/cart">
+                        <i className='fas fa-shopping-cart'></i>
+                        Cart
+                    </Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+        </header >
     )
 }
 
